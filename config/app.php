feea59c133fb9192e3edfa916c3b22f2ee50a801
liyingxuan <44600937@qq.com>
2016-04-04 19:17:00 +0800

    安装IDE代码提示插件。

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * IDE PHPStorm & Laravel Helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
