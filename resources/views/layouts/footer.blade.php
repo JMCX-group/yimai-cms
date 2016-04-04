13bdf85e1299494f9aff688c7ef64c261f0b3377
liyingxuan <44600937@qq.com>
2016-04-04 19:40:33 +0800

    基础界面搭建

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mvp_xuan
+ * Date: 2016-4-4
+ * Time: 19:22
+ */
+?>
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <strong>Copyright © {{date('Y')}} <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
